59a90d242bee67b4bfe0b7c5d0e660cddf0f8961
Gene Kogan <kogan.gene@gmail.com>
2023-05-01 10:30:49 -0700

    remove unecessary files

--- src/types/recorder-js.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'recorder-js' {
-  export default class Recorder {
-    constructor(audioContext: AudioContext);
-    init(stream: MediaStream): void;
-    start(): void;
-    stop(): Promise<{ blob: Blob }>;
-  }
-}
